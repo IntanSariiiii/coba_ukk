71677e7adc7f07b597170966ebf67e35fcb962e7
IntanSariiiii <intan.xpplg1@gmail.com>
2025-02-21 14:24:40 +0700

    ukk2025

--- lib/penjualan/keranjang.dart
+++ lib/penjualan/keranjang.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:ukk_2025/penjualan/indexpenjualan.dart';
-
+//penjualaan
 class AddTransaksi extends StatefulWidget {
   const AddTransaksi({super.key});
 
